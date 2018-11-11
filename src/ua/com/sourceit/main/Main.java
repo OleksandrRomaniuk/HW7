b64087ba79cadcbb9e88c1081772f324f3752d48
Oleksandr <alexandr.romaniuk@gmail.com>
2018-11-11 13:21:41 +0100

    second commit
    Signed-off-by: Oleksandr <alexandr.romaniuk@gmail.com>

---
+++ src/ua/com/sourceit/main/Main.java
@@ -0,0 +1,17 @@
+package ua.com.sourceit.main;
+
+public class Main
+{
+
+    public Main()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
